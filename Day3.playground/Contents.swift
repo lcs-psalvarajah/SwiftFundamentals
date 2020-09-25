dcad7e1ec5cb08aaf997197f54cc1c76d6c2df10
jina <psalvarajah@lcs.on.ca>
2020-09-25 12:20:41 -0400

    Did Day 3 Conditions (basically if and else statements)

--- Day3.playground/Contents.swift
+++ Day3.playground/Contents.swift
@@ -62,6 +62,23 @@ let twoScore = 4
 oneScore == twoScore
 oneScore != twoScore
 
-firstScore < secondScore
-firstScore >= secondScore
+oneScore < twoScore
+oneScore >= twoScore
+
+
+
+// Conditions
+
+let firstCard = 11
+let secondCard = 10
+
+
+
+if firstCard + secondCard == 2 {
+    print("Aces – lucky!")
+} else if firstCard + secondCard == 21 {
+    print("Blackjack!")
+} else {
+    print("Regular cards")
+}
 
