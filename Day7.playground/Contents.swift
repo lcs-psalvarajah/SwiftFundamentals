f1404d671bc90170c613ab0089349c51656a01a3
jina <psalvarajah@lcs.on.ca>
2020-10-05 11:54:54 -0400

    Day 7 completed Using closures as parameters when they accept parameters and Using closures as parameters when they return values

--- Day7.playground/Contents.swift
+++ Day7.playground/Contents.swift
@@ -9,5 +9,40 @@ Please [commit your work frequently](https://www.russellgordon.ca/cs/source-cont
 
 import Cocoa
 
-var str = "Hello, playground"
+
+// using closures when they accept parameters
+
+func travel(action: (String) -> Void) {
+    print("I'm getting ready to go")
+    action("Kenya")
+    print("I have arrived!")
+}
+
+travel { (place: String) in
+    print("Im going to \(place) in an airplane")
+}
+
+
+// using closures as parameters when they return values
+
+func traveltwo (action: (String) -> String) {
+    print("I'm getting ready to go")
+    let description = action("London")
+    print(description)
+    print("I have arrived!")
+}
+
+travel { (place: String ) -> String in
+    return "I'm going to \(place) in a plane"
+}
+
+
+//shorthand parameter names
+
+
+
+
+
+
+
 
