49c3d120f620d9d37292067276a8447c92c91de2
jina <psalvarajah@lcs.on.ca>
2020-10-13 14:55:42 -0400

    Day 8 finished tasks Creating your own structs, Computed properties, Property observers, and Methods

--- Day8.playground/Contents.swift
+++ Day8.playground/Contents.swift
@@ -9,4 +9,81 @@ Please [commit your work frequently](https://www.russellgordon.ca/cs/source-cont
 
 import Cocoa
 
-var str = "Hello, playground"
+// Creating your own structs
+
+struct Sport {
+    var name: String
+}
+
+var tennis = Sport(name: "Tennis")
+print(tennis.name)
+
+tennis.name = "Lawn tennis"
+
+
+// Computed properties
+
+struct sportTwo {
+    var name: String
+    var isOlympicSport: Bool
+
+    var olympicStatus: String {
+        if isOlympicSport {
+            return "\(name) is an Olympic sport"
+        } else {
+            return "\(name) is not an Olympic sport"
+        }
+    }
+}
+
+let chessBoxing = sportTwo(name: "Chessboxing", isOlympicSport: false)
+print(chessBoxing.olympicStatus)
+
+let gymnastics = sportTwo(name: "Gymnastics", isOlympicSport: true)
+print(gymnastics.olympicStatus)
+
+
+// Property observers
+
+
+var progress = Progress(task: "Loading data", amount: 0)
+progress.amount = 30
+progress.amount = 80
+progress.amount = 100
+
+struct Progress {
+    var task: String
+    var amount: Int {
+        didSet {
+            print("\(task) is now \(amount)% complete")
+        }
+    }
+}
+
+
+// Methods
+
+struct City {
+    var population: Int
+
+    func collectTaxes() -> Int {
+        return population * 1000
+    }
+}
+
+let kualaLumpar = City(population: 7_780_000)
+kualaLumpar.collectTaxes()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
