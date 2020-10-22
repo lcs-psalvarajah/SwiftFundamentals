ba34bd4ac1094299d549bf70b458548f48e0f004
jina <psalvarajah@lcs.on.ca>
2020-10-22 08:47:28 -0400

    Finished Day 10

--- Day10.playground/Contents.swift
+++ Day10.playground/Contents.swift
@@ -75,3 +75,59 @@ final class DogThree {
     }
 }
 
+
+
+// Copying objects
+
+class Singer {
+    var name = "Taylor Swift"
+}
+
+var singer = Singer()
+print(singer.name)
+
+var singerCopy = singer
+singerCopy.name = "Justin Bieber"
+
+print(singer.name)
+
+// Deinitializers
+
+class Person {
+    var name = "John Doe"
+
+    init() {
+        print("\(name) is alive!")
+    }
+
+    func printGreeting() {
+        print("Hello, I'm \(name)")
+    }
+    deinit {
+        print("\(name) is no more!")
+    }
+
+}
+
+for _ in 1...3 {
+    let person = Person()
+    person.printGreeting()
+}
+
+//Mutability
+
+//If you have a constant struct with a variable property, that property can’t be changed because the struct itself is constant.
+
+//However, if you have a constant class with a variable property, that property can be changed.
+
+class Singer {
+    let name = "Taylor Swift"
+}
+
+let taylor = Singer()
+taylor.name = "Ed Sheeran"
+print(taylor.name)
+
+
+
+
