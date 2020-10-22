65a1303072de5dab42f5f608118f096bd5f893ef
jina <psalvarajah@lcs.on.ca>
2020-10-22 09:05:15 -0400

    Finished Day 11

--- Day11.playground/Contents.swift
+++ Day11.playground/Contents.swift
@@ -17,6 +17,8 @@ protocol Identifiable {
     var id: String { get set }
 }
 
+//We can’t create instances of that protocol - it’s a description of what we want, rather than something we can create and use directly. But we can create a struct that conforms to it:
+
 struct User: Identifiable {
     var id: String
 }
@@ -24,6 +26,85 @@ struct User: Identifiable {
 func displayID(thing: Identifiable) {
     print("My ID is \(thing.id)")
 }
+
+//Protocol inheritance
+
+
+    //One protocol can inherit from another in a process known as protocol inheritance. Unlike with classes, you can inherit from multiple protocols at the same time before you add your own customizations on top.
+
+protocol Payable {
+    func calculateWages() -> Int
+}
+
+protocol NeedsTraining {
+    func study()
+}
+
+protocol HasVacation {
+    func takeVacation(days: Int)
+}
+
+protocol Employee: Payable, NeedsTraining, HasVacation { }
+
+
+//Extensions
+
+// Extensions allow you to add methods to existing types, to make them do things they weren’t originally designed to do.
+
+extension Int {
+    func squared() -> Int {
+        return self * self
+    }
+}
+
+let number = 8
+number.squared()
+
+extension Int {
+    var isEven: Bool {
+        return self % 2 == 0
+    }
+}
+
+//Protocol extensions
+
+// Protocols let you describe what methods something should have, but don’t provide the code inside. Extensions let you provide the code inside your methods, but only affect one data type – you can’t add the method to lots of types at the same time.
+
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+let beatles = Set(["John", "Paul", "George", "Ringo"])
+
+extension Collection {
+    func summarize() {
+        print("There are \(count) of us:")
+
+        for name in self {
+            print(name)
+        }
+    }
+}
+
+pythons.summarize()
+beatles.summarize()
+
+//Protocol-oriented programming
+
+protocol Identifiable2 {
+    var id: String { get set }
+    func identify()
+}
+
+extension Identifiable2 {
+    func identify() {
+        print("My ID is \(id).")
+    }
+}
+
+struct User2: Identifiable2 {
+    var id: String
+}
+
+let twostraws = User2(id: "twostraws")
+twostraws.identify()
 
 
 
